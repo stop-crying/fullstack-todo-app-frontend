ffe3687d970ef6844151e9b0e764e0ce340cc3c0
George Campbell <geocampo010@gmail.com>
2023-11-20 08:12:36 +0100

    v1.1.1

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>ToDo App</h1>
+        <h1>My Todo App</h1>
 
         <div className="top">
           <input
